bb3bb9b8e41c46a2ecabf7673ecfc4149bb16963
yaq1n0 <62781980+yaq1n0@users.noreply.github.com>
2026-04-21 06:32:30 +0100

    chore: fmt:fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { cloudflare } from '@cloudflare/vite-plugin';
 export default defineConfig({ plugins: [react(), tailwindcss(), cloudflare()] });
